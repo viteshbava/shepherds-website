488fc74f36ac8a30cfafc141c25990eaa63d60c4
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-03-01 00:40:16 +1300

    Add background clouds

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Barlow_Condensed } from 'next/font/google';
 import './globals.css';
 import Footer from './components/Footer';
+import Clouds from './components/Clouds';
 
 const baseFont = Barlow_Condensed({ subsets: ['latin'], weight: ['400', '700'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang='en'>
       <body
         className={`${baseFont.className} bg-shepherds_black text-shepherds_white h-svh min-h-[670px] flex flex-col items-center`}>
+        <Clouds />
         <main className='flex-grow w-10/12 md:w-auto'>{children}</main>
         <Footer />
       </body>
