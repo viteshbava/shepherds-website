c8b329408890fbb7b4a82e3ead99b5e51acb6a4e
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-10-10 13:19:33 -0400

    Add responsiveness to music

--- app/components/home/Music.tsx
+++ app/components/home/Music.tsx
@@ -7,12 +7,14 @@ const Music = () => {
   return (
     <Section id='music' className='mt-24'>
       <h2 className='text-2xl pb-2 w-full border-white/50 border-b-[1px]'>Music</h2>
-      <div className='flex flex-wrap justify-center w-full gap-4 mt-6'>
+      <div className='md:flex flex-wrap justify-center w-full gap-6 mt-6'>
         {[...musicList].reverse().map((release, index) => {
           const year = release?.releaseDate?.getFullYear();
           const caption = `${release.name}${year ? ` (${year})` : ''}`;
           return (
-            <div key={index} className='w-[calc((100%-1rem*2)/3)]'>
+            <div
+              key={index}
+              className='w-full md:w-[calc((100%-1.5rem*2)/2)] lg:w-[calc((100%-1.5rem*2)/3)] mb-5'>
               <div className='relative aspect-square w-full'>
                 <Image
                   priority
@@ -22,7 +24,7 @@ const Music = () => {
                   layout='fill'
                 />
               </div>
-              <p className='mt-1 w-full text-sm'>{caption}</p>
+              <p className='mt-1 w-full'>{caption}</p>
             </div>
           );
         })}
