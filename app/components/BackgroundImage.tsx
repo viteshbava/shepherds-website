b8fd74f624a2430c8b7e7de155f0d3cd54dca097
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-10-14 15:18:58 -0400

    Add transiition affect to background images

--- app/components/BackgroundImage.tsx
+++ app/components/BackgroundImage.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Image from 'next/image';
+import { useState } from 'react';
 
 interface BackgroundImageProps {
   url: string;
@@ -6,19 +9,24 @@ interface BackgroundImageProps {
 }
 
 const BackgroundImage = ({ url, altText }: BackgroundImageProps) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
-    <div className='absolute top-0 left-0 w-full min-h-svh'>
+    <div className='absolute top-0 left-0 w-full min-h-screen'>
       <Image
         priority
         src={url}
         alt={altText}
         fill
         sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-        className='absolute top-0 left-0 w-full object-cover opacity-40 blur-sm'
+        className={`absolute top-0 left-0 w-full object-cover blur-sm transition-opacity duration-300 ease-in-out ${
+          isLoaded ? 'opacity-40' : 'opacity-0'
+        }`}
         style={{
           maskImage: 'linear-gradient(to bottom, black 70%, transparent 100%)',
           WebkitMaskImage: 'linear-gradient(to bottom, black 70%, transparent 100%)',
         }}
+        onLoadingComplete={() => setIsLoaded(true)}
       />
     </div>
   );
