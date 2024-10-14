2fceb0df1bb34e66e25f15aa255ae866b5601ddf
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-10-14 17:29:07 -0400

    Adjust background image layout

--- app/components/BackgroundImage.tsx
+++ app/components/BackgroundImage.tsx
@@ -12,22 +12,24 @@ const BackgroundImage = ({ url, altText }: BackgroundImageProps) => {
   const [isLoaded, setIsLoaded] = useState(false);
 
   return (
-    <div className='absolute top-0 left-0 aspect-square w-full min-h-screen'>
-      <Image
-        priority
-        src={url}
-        alt={altText}
-        fill
-        sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-        className={`absolute top-0 left-0 w-full object-cover blur-sm transition-opacity duration-300 ease-in-out ${
-          isLoaded ? 'opacity-40' : 'opacity-0'
-        }`}
-        style={{
-          maskImage: 'linear-gradient(to bottom, black 70%, transparent 100%)',
-          WebkitMaskImage: 'linear-gradient(to bottom, black 70%, transparent 100%)',
-        }}
-        onLoadingComplete={() => setIsLoaded(true)}
-      />
+    <div className='absolute top-0 left-0 w-full min-h-screen overflow-hidden'>
+      <div className='aspect-square min-w-full min-h-full'>
+        <Image
+          priority
+          src={url}
+          alt={altText}
+          fill
+          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+          className={`absolute top-0 left-0 w-full object-cover blur-sm transition-opacity duration-300 ease-in-out ${
+            isLoaded ? 'opacity-40' : 'opacity-0'
+          }`}
+          style={{
+            maskImage: 'linear-gradient(to bottom, black 70%, transparent 100%)',
+            WebkitMaskImage: 'linear-gradient(to bottom, black 70%, transparent 100%)',
+          }}
+          onLoadingComplete={() => setIsLoaded(true)}
+        />
+      </div>
     </div>
   );
 };
