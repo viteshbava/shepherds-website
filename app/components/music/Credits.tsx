b9e8bcf87b950c9a08293bffec0dcb79d2c98a65
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-10-14 00:00:31 -0400

    Adjust padding on music page content

--- app/components/music/Credits.tsx
+++ app/components/music/Credits.tsx
@@ -10,7 +10,7 @@ interface CreditsProps {
 const Credits = ({ className = '', credits, theme }: CreditsProps) => {
   return (
     <div
-      className={`${className} flex flex-col justify-center items-center bg-black/10 backdrop-blur rounded px-4 py-6 sm:p-0 my-6 sm:my-0`}>
+      className={`${className} flex flex-col justify-center items-center bg-black/10 backdrop-blur rounded px-6 py-6 sm:p-0 my-6 sm:my-0`}>
       <h2
         className={`text-xl ${theme === Theme.Red ? 'text-red_bright' : 'text-green_bright'} mb-2`}>
         Credits
