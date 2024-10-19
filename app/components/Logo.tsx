2b7bcc9459b95dace45a0b4115b937fe6c18347c
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-10-20 12:35:29 +1300

    Add priority to logo

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -8,6 +8,7 @@ interface LogoProps {
 const Logo = ({ className = '' }: LogoProps) => {
   return (
     <Image
+      priority
       className={className}
       src='/imgs/soc-logo.png'
       alt='Shepherds of Cassini logo'
