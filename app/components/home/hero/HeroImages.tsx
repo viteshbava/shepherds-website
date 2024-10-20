8a9a89171de8454a4a3c1c64d7ec509e18c4ca85
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-10-20 18:26:06 +1300

    Update the hero images image logic

--- app/components/home/hero/HeroImages.tsx
+++ app/components/home/hero/HeroImages.tsx
@@ -16,10 +16,7 @@ const HeroImages = ({ images, altText, className = '' }: HeroImagesProps) => {
   const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
-    // Clear the interval if the component unmounts or dependencies change
     if (intervalId) clearInterval(intervalId);
-
-    // Start the loop when the image is loaded and not hovered
     if (images.length > 1 && loaded && !isHovered) {
       const newInterval = setInterval(() => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
@@ -33,15 +30,11 @@ const HeroImages = ({ images, altText, className = '' }: HeroImagesProps) => {
   }, [images.length, isHovered, loaded, currentIndex]);
 
   const onFirstImageLoad = () => {
-    // Trigger the loop as soon as the first image loads
     setLoaded(true);
   };
 
   const handleClick = () => {
-    // Move to the next image on click
     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-
-    // Restart the interval after the click, unless hovered
     if (intervalId) clearInterval(intervalId);
     if (!isHovered) {
       const newInterval = setInterval(() => {
@@ -55,12 +48,16 @@ const HeroImages = ({ images, altText, className = '' }: HeroImagesProps) => {
     <div
       className={`relative aspect-square max-w-full max-h-full w-full ${className}`}
       onMouseEnter={() => {
-        setIsHovered(true);
-        if (intervalId) clearInterval(intervalId); // Pause the loop when hovered
+        console.log('mouse entered');
+        const isTouchDevice = window.matchMedia('(hover: none)').matches;
+        if (!isTouchDevice) {
+          setIsHovered(true);
+          if (intervalId) clearInterval(intervalId);
+        }
       }}
       onMouseLeave={() => {
+        console.log('mouse left');
         setIsHovered(false);
-        // Resume the loop if not hovered
         if (!intervalId && loaded) {
           const newInterval = setInterval(() => {
             setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
@@ -80,7 +77,7 @@ const HeroImages = ({ images, altText, className = '' }: HeroImagesProps) => {
           fill
           sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
           onLoad={index === 0 ? onFirstImageLoad : undefined}
-          onClick={handleClick} // Click handler to go to the next image
+          onClick={handleClick}
         />
       ))}
     </div>
