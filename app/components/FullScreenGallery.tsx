2c841ed4e0d9e726deb9df3e38570c46436ccca6
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-10-21 13:12:19 +1300

    Improve accessiblity of full image gallery

--- app/components/FullScreenGallery.tsx
+++ app/components/FullScreenGallery.tsx
@@ -12,14 +12,18 @@ const FullscreenImage: React.FC<FullscreenImageProps> = ({ images, initialIndex,
   const [currentIndex, setCurrentIndex] = useState(initialIndex);
 
   useEffect(() => {
-    const handleEscape = (event: KeyboardEvent) => {
+    const handleKeydown = (event: KeyboardEvent) => {
       if (event.key === 'Escape') {
         onClose();
+      } else if (event.key === 'ArrowLeft') {
+        handleNavigation('prev');
+      } else if (event.key === 'ArrowRight') {
+        handleNavigation('next');
       }
     };
-    document.addEventListener('keydown', handleEscape);
+    document.addEventListener('keydown', handleKeydown);
     return () => {
-      document.removeEventListener('keydown', handleEscape);
+      document.removeEventListener('keydown', handleKeydown);
     };
   }, [onClose]);
 
@@ -37,7 +41,7 @@ const FullscreenImage: React.FC<FullscreenImageProps> = ({ images, initialIndex,
     <div className='fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-50'>
       <div className='fixed z-50 top-0 right-0 pr-2 h-[4.5rem] sm:h-24 flex flex-col justify-center'>
         <button
-          aria-label='Open menu'
+          aria-label='Close gallery'
           onClick={onClose}
           className='flex justify-center items-center focus:outline-none w-14 h-14 sm:w-16 sm:h-16 rounded-full transition'>
           <HamburgerButton closed={false} />
@@ -45,25 +49,22 @@ const FullscreenImage: React.FC<FullscreenImageProps> = ({ images, initialIndex,
       </div>
       <div className='relative z-40 max-w-4xl w-full h-[80vh] flex items-center justify-center'>
         <button
+          aria-label='Previous image'
           className='h-full px-4 text-white text-2xl'
           onClick={() => handleNavigation('prev')}>
           &#10094;
         </button>
         <div className='relative flex-grow w-full h-full'>
-          {images.map((image, index) => (
-            <Image
-              key={index}
-              src={image}
-              alt='Shepherds of Cassini gallery photo'
-              fill
-              sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-              className={`object-contain transition-opacity duration-300 ${
-                index === currentIndex ? 'opacity-100' : 'opacity-0'
-              }`}
-            />
-          ))}
+          <Image
+            src={images[currentIndex]}
+            alt='Shepherds of Cassini gallery photo'
+            fill
+            sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+            className='object-contain transition-opacity duration-300 opacity-100'
+          />
         </div>
         <button
+          aria-label='Next image'
           className='h-full px-4 text-white text-2xl'
           onClick={() => handleNavigation('next')}>
           &#10095;
