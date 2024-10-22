efbd205f3e1cff8334fa5af04dba51d0e62f9f31
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-10-22 15:03:15 +1300

    Add hover affects to full screen gallery buttons

--- app/components/FullScreenGallery.tsx
+++ app/components/FullScreenGallery.tsx
@@ -49,12 +49,14 @@ const FullscreenImage: React.FC<FullscreenImageProps> = ({ images, initialIndex,
         <button
           aria-label='Close gallery'
           onClick={onClose}
-          className='flex justify-center items-center focus:outline-none w-14 h-14 sm:w-16 sm:h-16 rounded-full transition'>
+          className='flex justify-center items-center focus:outline-none w-14 h-14 sm:w-16 sm:h-16 rounded-full transition hover:opacity-70'>
           <HamburgerButton closed={false} />
         </button>
       </div>
-      <button className='custom-prev absolute left-0 text-white text-3xl z-50 p-4'>&#10094;</button>
-      <button className='custom-next absolute right-0 text-white text-3xl z-50 p-4'>
+      <button className='custom-prev absolute left-0 text-white text-3xl z-50 p-4 hover:opacity-70'>
+        &#10094;
+      </button>
+      <button className='custom-next absolute right-0 text-white text-3xl z-50 p-4 hover:opacity-70'>
         &#10095;
       </button>
       <div className='relative z-40 max-w-4xl w-full h-[80vh] flex items-center justify-center'>
@@ -82,41 +84,6 @@ const FullscreenImage: React.FC<FullscreenImageProps> = ({ images, initialIndex,
       </div>
     </div>
   );
-  // return (
-  //   <div className='fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center z-50'>
-  //     <div className='fixed z-50 top-0 right-0 pr-2 h-[4.5rem] sm:h-24 flex flex-col justify-center'>
-  //       <button
-  //         aria-label='Close gallery'
-  //         onClick={onClose}
-  //         className='flex justify-center items-center focus:outline-none w-14 h-14 sm:w-16 sm:h-16 rounded-full transition'>
-  //         <HamburgerButton closed={false} />
-  //       </button>
-  //     </div>
-  //     <div className='relative z-40 max-w-4xl w-full h-[80vh] flex items-center justify-center'>
-  //       <button
-  //         aria-label='Previous image'
-  //         className='h-full px-4 text-white text-2xl'
-  //         onClick={() => handleNavigation('prev')}>
-  //         &#10094;
-  //       </button>
-  //       <div className='relative flex-grow w-full h-full'>
-  //         <Image
-  //           src={images[currentIndex]}
-  //           alt='Shepherds of Cassini gallery photo'
-  //           fill
-  //           sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-  //           className='object-contain transition-opacity duration-300 opacity-100'
-  //         />
-  //       </div>
-  //       <button
-  //         aria-label='Next image'
-  //         className='h-full px-4 text-white text-2xl'
-  //         onClick={() => handleNavigation('next')}>
-  //         &#10095;
-  //       </button>
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default FullscreenImage;
