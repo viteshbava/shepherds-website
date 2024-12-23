ff3b976c3d081ce6d24600950909092438e07131
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-12-23 17:20:17 +1300

    Add content to news section

--- app/components/home/News.tsx
+++ app/components/home/News.tsx
@@ -15,7 +15,7 @@ const News = () => {
           sizes='(max-width: 768px) 100vw, (max-width: 1920px) 50vw, 33vw'
         />
       </div>
-      <div className='relative w-full max-w-[500px] h-[100px] mx-auto'>
+      <div className='relative w-full max-w-[500px] h-[100px] mx-auto my-0 md:my-4'>
         <Image
           src='/imgs/heresy/album-logo.png'
           alt='Shepherds of Cassini, In Thrall to Heresy, album logo'
@@ -23,6 +23,22 @@ const News = () => {
           fill
           sizes='(max-width: 768px) 100vw, (max-width: 1920px) 50vw, 33vw'
         />
+      </div>
+      <div className='flex flex-col items-center max-w-xl mx-auto'>
+        <p className=''>
+          We&apos;re thrilled to reveal the name and cover of our new album:{' '}
+          <span className='italic'>In Thrall to Heresy</span>.
+        </p>
+        <p className='mt-6'>
+          <span className='text-red_bright'>Recorded & mixed by</span> Dave Rhodes<br></br>
+          <span className='text-red_bright'>Mastered by</span> Luke Finlay at Primal Mastering
+          <br></br>
+          <span className='text-red_bright'>Original artwork and lettering by</span> Moonroot
+        </p>
+        <p className='mt-6'>
+          More details coming soon - keep an eye out here or follow us on social media for updates.
+          Looking forward to sharing new music with the world again!
+        </p>
       </div>
     </Section>
   );
