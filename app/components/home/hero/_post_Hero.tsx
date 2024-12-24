b93fe50b1f6053a112cb53f9940dca83f8d0e3c4
viteshbava <82492234+viteshbava@users.noreply.github.com>
2024-12-24 13:35:20 +1300

    Fix type errors

--- app/components/home/hero/_post_Hero.tsx
+++ app/components/home/hero/_post_Hero.tsx
@@ -5,6 +5,7 @@ import ButtonLink from '../../ui/ButtonLink';
 import data from '@/app/data/music/heresy';
 import { getGalleryImages } from '@/app/libs/getGalleryImages';
 import MainImages from './HeroImages';
+import BandcampPlayer from '../../BandcampPlayer';
 
 const Hero = () => {
   const images = getGalleryImages({ folderPath: '/imgs/heresy/album-gallery' });
@@ -30,12 +31,7 @@ const Hero = () => {
           <h3 className='text-lg sm:text-xl mt-4 sm:mt-6 mb-3 text-red_bright'>
             Listen | Purchase
           </h3>
-          <div className='hidden sm:block w-full h-[120px]'>
-            <iframe className='border-0 w-full h-full' src={data.embed.large} seamless></iframe>
-          </div>
-          <div className='sm:hidden w-full h-[42px]'>
-            <iframe className='border-0 w-full h-full' src={data.embed.small} seamless></iframe>
-          </div>
+          <BandcampPlayer albumId={data.bandCampId} />
           <div className='flex flex-col space-y-3 mt-4'>
             {data.listenLinks.map((link, i) => (
               <ButtonLink
