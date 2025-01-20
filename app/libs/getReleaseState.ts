ee5ac5c6725fd80a6745780f8ea85e7b089b9fee
viteshbava <82492234+viteshbava@users.noreply.github.com>
2025-01-20 21:55:16 +1300

    Update test

--- app/libs/getReleaseState.ts
+++ app/libs/getReleaseState.ts
@@ -1,7 +1,7 @@
 type State = 'pre-single' | 'single-released' | 'album-released';
 
 export const getReleaseState = (): State => {
-  // UPDATE STATE HERE
+  // UPDATE STATE HERE!
   // const state: State = 'pre-single';
   const state: State = 'single-released';
   // const state: State = 'album-released';
