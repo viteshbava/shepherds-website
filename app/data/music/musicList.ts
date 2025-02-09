4411dfb12ccd7e3123999d7087748722f798d80a
viteshbava <82492234+viteshbava@users.noreply.github.com>
2025-02-09 17:49:47 +1300

    Re-instate heresy in data

--- app/data/music/musicList.ts
+++ app/data/music/musicList.ts
@@ -2,10 +2,14 @@ import { MusicData } from '@/app/types';
 import heresy from '@/app/data/music/heresy';
 import helios from '@/app/data/music/helios';
 import selftitled from '@/app/data/music/selftitled';
+import { getReleaseState } from '@/app/libs/getReleaseState';
 
 // This array controls the order of music releases.  Note that it may be reversed on the UI depending on context.
 // Remove an album from this array to hide it
-const musicList: MusicData[] = [selftitled, helios];
-// const musicList: MusicData[] = [selftitled, helios, heresy];
+
+const releaseState = getReleaseState();
+
+const musicList: MusicData[] =
+  releaseState === 'album-released' ? [selftitled, helios, heresy] : [selftitled, helios];
 
 export default musicList;
--- app/libs/getReleaseState.ts
+++ app/libs/getReleaseState.ts
@@ -3,7 +3,7 @@ type State = 'pre-single' | 'single-released' | 'album-released';
 export const getReleaseState = (): State => {
   // UPDATE STATE HERE!
   // const state: State = 'pre-single';
-  const state: State = 'single-released';
-  // const state: State = 'album-released';
+  // const state: State = 'single-released';
+  const state: State = 'album-released';
   return state;
 };
