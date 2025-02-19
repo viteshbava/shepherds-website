a2ab9574c7a8647a310f467a644cc9372055f062
viteshbava <82492234+viteshbava@users.noreply.github.com>
2025-02-19 18:56:35 +1300

    Add bandcamp detail to purchase page

--- app/components/purchase/ShippingInfo.tsx
+++ app/components/purchase/ShippingInfo.tsx
@@ -3,39 +3,45 @@ import { FaCompactDisc, FaRecordVinyl } from 'react-icons/fa';
 
 const ShippingInfo = () => {
   return (
-    <div className='flex gap-4 items-center'>
-      <div className='p-4 border border-gray-200 rounded-lg '>
-        <div className='flex items-center text-lg font-semibold text-red-900'>
+    <div className='grid md:flex justify-center grid-cols-2 gap-4 w-full'>
+      <div className='p-4 border border-gray-200 rounded-lg'>
+        <div className='flex items-center justify-center text-lg font-semibold text-red-900 mb-4'>
           <FaRecordVinyl className='mr-2 text-3xl' />
           Vinyl
         </div>
         <div className='mt-2 text-sm'>
-          <p>
-            📦 <strong>NZ:</strong> Up to $20
+          <p className='mb-2'>
+            📦 <span className='font-bold'>NZ:</span>{' '}
+            <span className='block sm:inline'>Up to $20</span>
           </p>
-          <p>
-            📦 <strong>Australia:</strong> Up to $35
+          <p className='mb-2'>
+            📦 <span className='font-bold'>Australia:</span>{' '}
+            <span className='block sm:inline'>Up to $35</span>
           </p>
-          <p>
-            📦 <strong>International:</strong> Up to $60
+          <p className='mb-2'>
+            📦 <span className='font-bold'>International:</span>{' '}
+            <span className='block sm:inline'>Up to $60</span>
           </p>
         </div>
       </div>
 
       <div className='p-4 border border-gray-200 rounded-lg'>
-        <div className='flex items-center text-lg font-semibold text-red-900'>
+        <div className='flex items-center justify-center text-lg font-semibold text-red-900 mb-4'>
           <FaCompactDisc className='mr-2 text-3xl' />
           CD
         </div>
         <div className='mt-2 text-sm'>
-          <p>
-            📦 <strong>NZ:</strong> Up to $10
+          <p className='mb-2'>
+            📦 <span className='font-bold'>NZ:</span>{' '}
+            <span className='block sm:inline'>Up to $10</span>
           </p>
-          <p>
-            📦 <strong>Australia:</strong> Up to $25
+          <p className='mb-2'>
+            📦 <span className='font-bold'>Australia:</span>{' '}
+            <span className='block sm:inline'>Up to $25</span>
           </p>
-          <p>
-            📦 <strong>International:</strong> Up to $35
+          <p className='mb-2'>
+            📦 <span className='font-bold'>International:</span>{' '}
+            <span className='block sm:inline'>Up to $35</span>
           </p>
         </div>
       </div>
--- app/purchase/page.tsx
+++ app/purchase/page.tsx
@@ -9,10 +9,11 @@ import {
 import BackgroundImage from '../components/BackgroundImage';
 import Image from 'next/image';
 import { getGalleryImages } from '../libs/getGalleryImages';
-import { Product } from '../types';
+import { IconNames, Product, Theme } from '../types';
 import ProductList from '../components/purchase/ProductList';
 import ShippingTable from '../components/purchase/ShippingTable';
 import ShippingInfo from '../components/purchase/ShippingInfo';
+import ButtonLink from '../components/ui/ButtonLink';
 
 const products: Product[] = [
   {
@@ -22,7 +23,8 @@ const products: Product[] = [
     productType: '2LP Vinyl',
     price: 'NZD $80',
     priceInfo: 'directly from band',
-    description: 'The full album on LIMITED EDITION black double vinyl',
+    description:
+      'The full album on LIMITED EDITION black double vinyl. Products due end of Feb 2025.',
     details: ['Gatefold jacket', '2 sided lyric insert', 'Original art by Moonroot'],
   },
   {
@@ -79,28 +81,17 @@ const Purchase = () => {
         {/* <ShippingTable /> */}
         <ShippingInfo />
         <p className='text-sm mt-2 italic'>All prices are in NZD.</p>
-        <p className='max-w-xl mt-8 mb-4'>
-          Shipping prices above are for a single item only. If you&#39;d like more items, we&#39;ll
-          calculate the cost for you. Once you contact us, we&#39;ll reach out with final pricing
-          details and how to make the payment.
-        </p>
 
-        <div className='max-w-xl mt-10'>
+        <div className='max-w-xl mt-20'>
           <h2 className='text-lg sm:text-2xl gold-heading pb-4 border-white/50 border-b-[1px] w-full'>
             Directly from band
           </h2>
-          <p className='my-6'>
-            Our new album <span className='font-semibold italic'>In Thrall to Heresy</span> will be
-            released on{' '}
-            <span className='font-semibold'>
-              21<sup>st</sup> February 2025
-            </span>{' '}
-            and will be available on 2LP Vinyl and CD.
-          </p>
 
           <div
-            className={`flex flex-col justify-center items-center bg-red_dim/10 backdrop-blur rounded-md p-8 mb-6`}>
-            <p className='mb-6'>To make a purchase, please contact us directly via:</p>
+            className={`flex flex-col justify-center items-center bg-red_dim/10 backdrop-blur rounded-md p-8 my-6`}>
+            <p className='mb-6'>
+              To purchase directly from us, please contact us via any of the following:
+            </p>
             <div>
               <p className='mb-2'>
                 <FaEnvelope className='text-red-900 text-2xl inline-block mr-2' /> Email:{' '}
@@ -121,22 +112,39 @@ const Purchase = () => {
             </div>
           </div>
 
-          <p className='mb-4'>
-            If you&#39;re based in Auckland, we may be able to deliver to you directly without
-            shipping costs. Let us know where you&#39;d like your order delivered or where
-            you&#39;re based when you contact us.
+          <p className='max-w-xl mt-8 mb-4'>
+            Prices above are for a single item only. If you&#39;d like more items, let us know and
+            we&#39;ll calculate the cost for you. Once you contact us, we&#39;ll reach out with
+            final pricing details and how to make the payment.
           </p>
 
           <p className='mb-4'>
-            Albums are expected to be ready near the release date; we&#39;ll ship orders out around
-            then. If there are any delays, we&#39;ll keep you informed.
+            <span className='underline'>
+              If you&#39;re based in Auckland, we may be able to deliver to you directly without
+              shipping costs.
+            </span>{' '}
+            Let us know where you&#39;d like your order delivered or where you&#39;re based when you
+            contact us.
           </p>
 
-          <p className='mb-8'>Orders for physical albums on Bandcamp will open at a later point.</p>
-
-          <h2 className='text-lg sm:text-2xl gold-heading pb-4 border-white/50 border-b-[1px] w-full'>
+          <h2 className='text-lg sm:text-2xl gold-heading mt-20 pb-4 border-white/50 border-b-[1px] w-full'>
             via bandcamp
           </h2>
+
+          <p className='my-6'>
+            Alternavitely, you can make a purchase via our Bandcamp page (prices may be slightly
+            different to account for Bandcamp / payment processer fees).
+          </p>
+
+          <ButtonLink
+            isExternal
+            href={'https://shepherdsofcassini.bandcamp.com/merch'}
+            ariaLabel='Purchase via Bandcamp'
+            theme={Theme.Red}
+            icon={IconNames.Bandcamp}
+            className={`w-full mb-20`}>
+            <span className='whitespace-nowrap'>Purchase on Bandcamp</span>
+          </ButtonLink>
 
           <p className='text-2xl mb-4'>Thank you!</p>
         </div>
